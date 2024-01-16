ae96ca026848a5baf7b099e9ea1e4067669c3557
yashika-salonia <yashikasalonia20@gmail.com>
2024-01-16 23:26:51 +0530

    Add file

---
+++ AIC-JavaScript/WEEK-1/DAY-1/operator.js
@@ -0,0 +1,30 @@
+//Operators in Js
+
+//Arithmetic Operator
+//Addition '+'
+let x=5;
+let y=6;
+let z=x+y;
+
+//Subtraction '-'
+let s=y-x;
+
+//Multiplication '*'
+let r=x*y;
+
+//Assignment '='
+let m=10;
+let n=x+y;
+
+//many more like division, modulusincrement,decrement etc.
+
+//Assignment operator: '=', '+=', '-=', '*=', '/=' and many more
+
+let a=10;
+a += 5; // a=a+5
+
+ //String operator
+ let text1="hi";
+ let text2="welcome";
+ let text3=text1+" "+text2;
+ console.log(text3);
---
+++ AIC-JavaScript/WEEK-1/DAY-1/varriable.js
@@ -0,0 +1,32 @@
+//Here is the example of some variable a,b and c which are aiigned dome values.
+var a=20;
+var b="ABCXYZ";
+var c=34.67;
+console.log(a);
+console.log(b);
+console.log(c);
+
+//Here are some data types like 
+// Numbers
+let length = 72;
+let weight = 7.5;
+
+// Strings
+let color = "green";
+let lastName ="singh";
+
+// Booleans
+let x = 5;
+let y = 5;
+let z = 6;
+(x==y) //true
+(x==z) //false
+
+// Object
+const person = {firstName:"Raj", lastName:"kumar"};
+
+// Array object
+const cars = ["Swift", "Thar", "BMW"];
+
+// Date object
+const date = new Date("2024-01-16");
