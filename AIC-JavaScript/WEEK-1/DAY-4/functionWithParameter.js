5096b05a4de4cf15a8abd469a347ef408b8f1475
yashika-salonia <yashikasalonia20@gmail.com>
2024-01-19 21:32:34 +0530

    Add file

---
+++ AIC-JavaScript/WEEK-1/DAY-4/functionWithParameter.js
@@ -0,0 +1,16 @@
+//Function with parameters
+
+//Sum function with parameters defined
+function sum(x,y,z){
+    let s=x+y+z;
+    return "The sum of two numbers are " + s;
+}
+console.log(sum(20,70,64));
+
+//Division function
+function div(p,q){
+    let d=p/q;
+    return "The value of div of two digits are "+ d;
+}
+console.log(div(220,6));
+
---
+++ AIC-JavaScript/WEEK-1/DAY-4/functionWithoutParameter.js
@@ -0,0 +1,18 @@
+//Function without parameters
+
+//Square function without parameters
+function square(){
+    var x = 2;
+    return "The square of two numbers is " + x*x;
+}
+console.log(square());
+
+//Product function without parameters
+function product(){
+    var a=10;
+    var b=3;
+    var c=7;
+    var prod=a*b*c;
+    return "The product of two numbers is " + prod;
+}
+console.log(product());
